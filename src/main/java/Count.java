7ecffbb9d2e764de765c700cc01df312e84f3d44
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-10-31 15:27:35 -0500

    servlets exercise init

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -1,24 +1,22 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "Count", urlPatterns = "/count")
+public class Count extends HttpServlet {
 
+    int count = 0;
 
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        name = (name == null) ? "wolrd" : name;
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello," + name + "</h1>");
+        count++;
+        out.println("<h1>Count = " + count + "</h1>");
 
     }
-
 }
-
---
+++ src/main/java/RegisterServlet.java
@@ -0,0 +1,10 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
