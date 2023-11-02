66cd6bb27c9b7af1e90dcf682c69f25675349604
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-11-02 17:01:09 -0500

    Sessions and Cookies exercise init

--- src/main/java/LogOutServlet.java
+++ src/main/java/LogOutServlet.java
@@ -6,15 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        if (session.getAttribute("user") == null){
-            response.sendRedirect("/login");
-        } else {
-            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        }
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogOutServlet extends HttpServlet {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("/login");
     }
 }
